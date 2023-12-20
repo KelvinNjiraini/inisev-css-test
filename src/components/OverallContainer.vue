33b0beb5efe9ecd6c73deecffec224a347517267
Kelvin Njiraini <njirainikevo46@gmail.com>
2023-12-20 16:48:44 +0300

    Rating complete version

--- src/components/OverallContainer.vue
+++ src/components/OverallContainer.vue
@@ -1,6 +1,9 @@
 <script setup>
+import { ref } from 'vue';
 import CardMain from './CardMain.vue';
 import RatingBox from './RatingBox.vue';
+
+const ratingsComplete = ref(true);
 </script>
 
 <template>
@@ -13,19 +16,11 @@ import RatingBox from './RatingBox.vue';
                 <div class="flex items-center space-x-2">
                     <!-- quick rating mode -->
 
-                    <svg
-                        width="28"
-                        height="28"
-                        viewBox="0 0 28 28"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
+                    <img
+                        src="./../assets/icons/lightning-bolt.svg"
+                        alt="Ligtning bolt"
                         class="w-8 h-8 md:w-10 md:h-10"
-                    >
-                        <path
-                            d="M21.8894 9.60307C21.8129 9.43879 21.6481 9.33335 21.4666 9.33335H15.9366L21.3943 0.716352C21.4853 0.572633 21.4908 0.390633 21.4088 0.241773C21.3266 0.0924219 21.1698 0 20.9999 0H13.5333C13.3564 0 13.1949 0.0998594 13.1156 0.25807L6.11561 14.2581C6.04326 14.4023 6.05119 14.574 6.13612 14.7116C6.22154 14.8493 6.37133 14.9333 6.53326 14.9333H11.332L6.10298 27.3522C6.01384 27.5646 6.09319 27.8109 6.28963 27.9314C6.36521 27.9776 6.44921 27.9999 6.53277 27.9999C6.6667 27.9999 6.79877 27.9425 6.89026 27.8343L21.8236 10.1009C21.9407 9.96193 21.9659 9.76779 21.8894 9.60307Z"
-                            fill="#E4B200"
-                        />
-                    </svg>
+                    />
                     <span
                         class="font-semibold text-2xl lg:text-3xl text-slate-900"
                     >
@@ -38,37 +33,18 @@ import RatingBox from './RatingBox.vue';
                 >
 
                 <Button class="outline-none inline-block">
-                    <svg
-                        width="30"
-                        height="30"
-                        viewBox="0 0 30 30"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
+                    <img
+                        src="./../assets/icons/cross.svg"
+                        alt="cross"
                         class="w-8 h-8 md:w-10 md:h-10"
-                    >
-                        <path
-                            d="M18.9676 18.9674L11.5002 11.5"
-                            stroke="#ABBBCE"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                        <path
-                            d="M11.5026 18.9674L18.9699 11.5"
-                            stroke="#ABBBCE"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                        <circle cx="15" cy="15" r="14.5" stroke="#D5DDE7" />
-                    </svg>
+                    />
                 </Button>
             </div>
 
             <!-- prev and next button on mobile screens -->
-            <div class="flex w-full justify-between items-center xs:hidden">
+            <div class="grid grid-cols-3 items-center w-full xs:hidden">
                 <Button
-                    class="outline-none scale-100 hover:scale-[1.02] transition-all duration-75"
+                    class="outline-none scale-100 hover:scale-[1.02] transition-all duration-75 justify-self-start"
                 >
                     <img
                         src="./../assets/icons/chevron-left.svg"
@@ -76,11 +52,13 @@ import RatingBox from './RatingBox.vue';
                         class="w-10 h-10"
                     />
                 </Button>
-                <span class="font-semibold text-lg text-light-gray"
+                <span
+                    class="font-semibold text-lg text-light-gray justify-self-center"
                     ><span class="text-color-green">1</span>/50 topics</span
                 >
                 <Button
-                    class="outline-none scale-100 hover:scale-[1.02] transition-all duration-75"
+                    class="outline-none scale-100 hover:scale-[1.02] transition-all duration-75 justify-self-end"
+                    v-if="!ratingsComplete"
                 >
                     <img
                         src="./../assets/icons/chevron-right.svg"
@@ -89,9 +67,12 @@ import RatingBox from './RatingBox.vue';
                     />
                 </Button>
             </div>
-            <div class="flex items-center justify-between space-x-3">
+            <!-- prev and next button on larger screens -->
+            <div
+                class="grid grid-cols-1 xs:grid-cols-[repeat(3,auto)] gap-x-2 sm:gap-x-4 items-center w-full"
+            >
                 <Button
-                    class="outline-none scale-100 hover:scale-[1.02] transition-all duration-75 hidden xs:inline-block"
+                    class="outline-none scale-100 hover:scale-[1.02] transition-all duration-75 hidden xs:inline-block justify-self-start"
                 >
                     <img
                         src="./../assets/icons/chevron-left.svg"
@@ -100,14 +81,44 @@ import RatingBox from './RatingBox.vue';
                     />
                 </Button>
                 <div
-                    class="flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 lg:space-x-16 w-full"
+                    class="justify-self-stretch flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 lg:space-x-16 w-full"
+                    v-if="!ratingsComplete"
                 >
                     <!-- <div class="md:w-3/5"> -->
                     <card-main />
                     <!-- </div> -->
                     <RatingBox />
                 </div>
-                <Button class="outline-none hidden xs:inline-block">
+                <div
+                    class="justify-self-stretch flex flex-col items-center space-y-4 w-full md:py-5"
+                    v-if="ratingsComplete"
+                >
+                    <!-- Completion screen -->
+                    <img
+                        src="./../assets/celebrate-symbol.svg"
+                        alt="celebrate symbol"
+                        class="h-[9rem]"
+                    />
+                    <h2
+                        class="text-3xl text-slate-900 font-semibold text-center max-w-sm"
+                    >
+                        You rated all topics that could be rated – hurray!
+                    </h2>
+                    <button
+                        class="rounded-xl bg-color-green px-4 py-3 text-white font-semibold hover:bg-color-green/80"
+                    >
+                        Close quick rating mode
+                    </button>
+                    <button
+                        class="rounded-xl outline-none px-4 py-3 text-color-green hover:text-color-green/80 font-semibold"
+                    >
+                        Start from beginning
+                    </button>
+                </div>
+                <Button
+                    class="outline-none scale-100 hover:scale-[1.02] transition-all duration-75 hidden xs:inline-block justify-self-end"
+                    v-if="!ratingsComplete"
+                >
                     <img
                         src="./../assets/icons/chevron-right.svg"
                         alt="chevron right"
